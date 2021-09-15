8dba369d257df1449e22c3888f179d01a8f28c3a
RandomPersonNumeroDos <90705768+RandomPersonNumeroDos@users.noreply.github.com>
2021-09-15 09:54:05 -0400

    Add files via upload

---
+++ builtInSets.js
@@ -0,0 +1,35 @@
+var builtInSets = [
+   {
+        name: "Duskit",
+        setName: "Peace of Mind",
+        evs: {
+            hp: 100,
+            energy: 0,
+            attack: 0,
+            defense: 0,
+            attackR: 200,
+            defenseR: 0,
+            speed: 200
+        },
+        ivs: {
+            hp: 40,
+            energy: 40,
+            attack: 40,
+            defense: 40,
+            attackR: 40,
+            defenseR: 40,
+            speed: 40
+        },
+        moves: {
+            move1: "Spectral Burst",
+            move2: "Brainwash",
+            move3: "Peace Of Mind",
+            move4: "Final Ruse"
+        },
+        posNature: "nimble",
+        negNature: "none",
+        ability: "None",
+        item: "Power Cuffs",
+        level: 50
+    }
+];
